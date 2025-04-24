3051322ab6152948b4a81da3ced7f818f64f223d
rge <rge9196@gmail.com>
2025-04-23 19:59:01 -0600

    after 08

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
-const convertToCelsius = function(num) {
-  return Math.round((num-32)*5/9*10)/10
+const convertToCelsius = function (num) {
+  return Math.round((((num - 32) * 5) / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function(num) {
-  return Math.round((num*9/5+32)*10)/10
+const convertToFahrenheit = function (num) {
+  return Math.round(((num * 9) / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,25 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let subSum = 0;
+  for (const num of arr) {
+    subSum = subSum + num;
+    console.log(subSum);
+  }
+  return subSum;
 };
 
-const multiply = function() {
+const multiply = function () {};
 
-};
+const power = function () {};
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = function () {};
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +28,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
