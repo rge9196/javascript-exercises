a7a8c5d427b93419f2bfbc1d34d261f79f3e8bfb
rge <rge9196@gmail.com>
2025-05-01 12:30:05 -0600

    after fib

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function (input) {
+  let num = Number(input); // → 2
 
+  // Step 1: Check if num is negative
+  if (num < 0) {
+    return "OOPS";
+  }
+
+  if (num == 0) {
+    return 0;
+  }
+
+  // Step 2: If num is 1 or 2, return 1
+  if (num === 1 || num === 2) {
+    return 1;
+  }
+  let last = 1;
+  let current = 1;
+  let temp = 0;
+
+  // Step 3: Loop from 3 to num
+  for (let i = 3; i <= num; i++) {
+    temp = current;
+    current = current + last;
+    last = temp;
+  }
+  return current;
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
