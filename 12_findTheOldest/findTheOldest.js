cffe33a75c72c9b5476d1525a206b272f6f51cf1
rge <rge9196@gmail.com>
2025-05-01 13:16:31 -0600

    after find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const currentYear = new Date().getFullYear();
+  let oldestPerson = "";
+  let oldestAge = 0;
 
+  for (const person of people) {
+    let name = person.name;
+    let birth = person.yearOfBirth;
+    let death = "yearOfDeath" in person ? person.yearOfDeath : currentYear;
+    let age = death - birth;
+
+    if (age > oldestAge) {
+      oldestAge = age;
+      oldestPerson = name;
+    }
+  }
+
+  return { name: oldestPerson, age: oldestAge };
 };
 
 // Do not edit below this line
